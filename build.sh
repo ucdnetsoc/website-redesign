3a1d386e4e28d59f2f0ba2ae64644eaa54edb12a
Hari <hprasad2709@gmail.com>
2024-09-06 13:45:15 +0100

    Updated build script to use pushd/popd

--- build.sh
+++ build.sh
@@ -1,9 +1,10 @@
 #! /usr/bin/bash
 
-cd /tmp
+pushd /tmp
 git clone https://github.com/ucdnetsoc/website-redesign
 cd website-redesign
 mkdir dist
 docker build -t homebuild -f Dockerfile .
 docker run --rm -v "/tmp/website-redesign/dist:/site/dist" homebuild
 mv /tmp/homepage /opt/static/next.netsoc.com
+popd
